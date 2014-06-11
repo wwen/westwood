82fc0c7ca5338848256f2b6a10cdcd7d3aae3222
若鸣 <jackson1984813@Tings-Mac.local>
2014-06-12 08:01:56 +1200

    auto-slider complete

--- media/js/views/portfolio/portfolioView.js
+++ media/js/views/portfolio/portfolioView.js
@@ -25,9 +25,84 @@ define(['jquery',
 	   				var that = this;
 
 	   				_.defer(function() {
-	   					
+	   					that.imgLoader();
 	   				});
 	   			},
+
+	   			imgLoader: function() {
+	   				//first time to get images by ajax call
+	   				var oRequest = new XMLHttpRequest();
+	   				var imageObj = {};
+	   				var list = $("#image_container li");
+	   				var num = 4;
+	   				//console.log(list.eq(1));
+
+	   				oRequest.open("get", "./media/js/views/portfolio/image.json", false);
+	   				oRequest.setRequestHeader("Content-type", "application/json");
+
+	   				oRequest.onreadystatechange = function() {
+	   					if(oRequest.readyState == 4 && oRequest.status == 200) {
+	   						imageObj = JSON.parse(oRequest.responseText);
+	   					}
+	   				}
+	   				
+	   				oRequest.send(null);
+
+	   				//initialize image, loading 4 images at first time
+   					for (var i=0; i<4; i++) {
+	   					var imgtmpl = '<img src="' + imageObj[i].after + '" alt="westwood" />';
+	   					list.eq(i).append(imgtmpl);
+	   					list.eq(i).css({left: i*750+"px"});
+	   					if(list.eq(i).css("left") == "0px") {
+	   						list.eq(i).addClass("image_active");
+	   					}
+	   				}
+	   				
+	   				this.slide(imageObj, num, list);
+	   			},
+
+	   			//automatic slide
+	   			slide: function(image, num, list) {
+	   				var imgNum = image.length;
+
+					var set = window.setInterval(function() {
+						var count = 0;
+
+						list.animate({left: "-=750px"}, 500, "linear", function() {
+							//Because ist has 4 elements, the callback function of animate 
+							//will execute 4 times. So set a count to let callback function 
+							//just execute 1 time.
+							count++;
+							while(count == 1){
+								if(list.eq(0).css("left") == "-750px") {
+									//remove the first image
+									list.eq(0).addClass("remove");
+									$(".remove").remove();
+									//add new image to the lists, 1 image per time
+									list.last().after('<li style="left: 2250px;"><img src="' + image[num].after + '" alt="westwood" /></li>');
+									num++;
+									//re-get list array
+									list = $("#image_container li");
+									if (num == imgNum) {
+										num = 0;
+									}
+								}
+								break;
+							}
+						});
+						
+						// reset all the images position, work for if loading all images 1 time
+						/*if ($("#image_container li").css("left") == "-1500px") {
+							window.setTimeout(function() {
+								list.animate({left: "+=2250px"}, 500, "linear");
+								clearInterval(set);
+								slide();
+							}, 2000);
+						}*/
+					}, 5000);
+	   			}
+
+	   			
 	   		});	
 
 	   		return PortfolioView;
